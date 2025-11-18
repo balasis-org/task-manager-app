1bcd385a7f79a98ac2add6c46f083f650124007b
Balasis_Ioannis <giovani1994a@gmail.com>
2025-11-18 15:21:13 +0200

    [fix] unify managed identity by injectable bean and change of nginx.dev.conf redirect port

--- task-manager-backend/task-manager-engine/task-manager-engine-infrastructure/src/main/java/io/github/balasis/taskmanager/engine/infrastructure/secret/azure/KeyVaultConfig.java
+++ task-manager-backend/task-manager-engine/task-manager-engine-infrastructure/src/main/java/io/github/balasis/taskmanager/engine/infrastructure/secret/azure/KeyVaultConfig.java
@@ -1,29 +1,27 @@
 package io.github.balasis.taskmanager.engine.infrastructure.secret.azure;
 
 import com.azure.core.credential.TokenCredential;
+import com.azure.identity.ManagedIdentityCredential;
 import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile({"prod-h2","prod-azuresql"})
 @Configuration
+@AllArgsConstructor
 public class KeyVaultConfig {
+    private final ManagedIdentityCredential managedIdentityCredential;
 
     @Bean
     public SecretClient secretClient() {
-        String keyVaultUri = System.getenv("KEYVAULT_URI");
-        String managedIdentityClientId = System.getenv("MANAGED_IDENTITY_CLIENT_ID");
-
-        TokenCredential credential = new ManagedIdentityCredentialBuilder()
-                .clientId(managedIdentityClientId)
-                .build();
 
         return new SecretClientBuilder()
-                .vaultUrl(keyVaultUri)
-                .credential(credential)
+                .vaultUrl(System.getenv("KEYVAULT_URI"))
+                .credential(managedIdentityCredential)
                 .buildClient();
     }
 }
