ecd1ec0db779b8026a355ed17d90793d4c2047b0
Balasis_Ioannis <giovani1994a@gmail.com>
2025-11-21 00:35:49 +0200

    [feat] azure AD auth flow Part 1/2 (dev tested)

---
+++ task-manager-backend/task-manager-engine/task-manager-engine-core/src/main/java/io/github/balasis/taskmanager/engine/core/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package io.github.balasis.taskmanager.engine.core.repository;
+
+import io.github.balasis.taskmanager.context.base.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByAzureId(String azureId);
+}
