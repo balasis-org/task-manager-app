865e241a2b04df6d6f76d7836cda15ce62da7592
Balasis_Ioannis <giovani1994a@gmail.com>
2025-12-24 09:50:11 +0200

    [feat] refactor blob service; support multiple containers and delete of TaskFileDeleted fallback

--- task-manager-backend/task-manager-engine/task-manager-engine-infrastructure/src/main/java/io/github/balasis/taskmanager/engine/infrastructure/blob/service/BlobStorageService.java
+++ task-manager-backend/task-manager-engine/task-manager-engine-infrastructure/src/main/java/io/github/balasis/taskmanager/engine/infrastructure/blob/service/BlobStorageService.java
@@ -4,6 +4,7 @@ package io.github.balasis.taskmanager.engine.infrastructure.blob.service;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.models.PublicAccessType;
 import io.github.balasis.taskmanager.engine.infrastructure.secret.SecretClientProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,28 +13,51 @@ import java.io.IOException;
 import java.util.UUID;
 
 @Service
-public class BlobStorageService {//TODO: change await to response time? to avoid delay of deletion if down?
-    private final BlobContainerClient containerClient;
+public class BlobStorageService {
 
-    public BlobStorageService(SecretClientProvider secretClientProvider, BlobServiceClient blobServiceClient) {
-        String containerName = secretClientProvider.getSecret("TASKMANAGER-BLOB-CONTAINERS-NAME");
-        this.containerClient = blobServiceClient.getBlobContainerClient(containerName);
-        if (!containerClient.exists()) {
-            containerClient.create();
+    private static final String TASK_FILE_CONTAINER = "task-files";
+    private static final String PROFILE_IMG_CONTAINER = "profile-images";
+    private static final String GROUP_IMG_CONTAINER = "group-images";
+    private static final String TASK_IMG_CONTAINER = "task-images";
+
+    private final BlobContainerClient taskFileContainer;
+    private final BlobContainerClient profileImgContainer;
+    private final BlobContainerClient groupImgContainer;
+    private final BlobContainerClient taskImgContainer;
+
+    public BlobStorageService(BlobServiceClient blobServiceClient) {
+        this.taskFileContainer = initContainer(blobServiceClient, TASK_FILE_CONTAINER, false);
+        this.profileImgContainer = initContainer(blobServiceClient, PROFILE_IMG_CONTAINER, true);
+        this.groupImgContainer = initContainer(blobServiceClient, GROUP_IMG_CONTAINER, true);
+        this.taskImgContainer = initContainer(blobServiceClient, TASK_IMG_CONTAINER, true);
+    }
+
+    private BlobContainerClient initContainer(
+            BlobServiceClient serviceClient,
+            String containerName,
+            boolean isPublic
+    ) {
+        BlobContainerClient container = serviceClient.getBlobContainerClient(containerName);
+
+        if (!container.exists()) {
+            container.create();
+            if (isPublic) {
+                container.setAccessPolicy(PublicAccessType.BLOB, null);
+            }
         }
+
+        return container;
     }
 
-    public String upload(MultipartFile file) throws IOException {
+    private String uploadInternal(BlobContainerClient container, MultipartFile file) throws IOException {
         String blobName = UUID.randomUUID() + "-" + file.getOriginalFilename();
-        BlobClient blobClient = containerClient.getBlobClient(blobName);
+        BlobClient blobClient = container.getBlobClient(blobName);
         blobClient.upload(file.getInputStream(), file.getSize(), true);
-        String fullUrl = blobClient.getBlobUrl();
-        return fullUrl.substring(fullUrl.indexOf(containerClient.getBlobContainerName())
-                + containerClient.getBlobContainerName().length() + 1);
+        return blobName;
     }
 
-    public byte[] download(String blobName) throws IOException {
-        BlobClient blobClient = containerClient.getBlobClient(blobName);
+    private byte[] downloadInternal(BlobContainerClient container, String blobName) throws IOException {
+        BlobClient blobClient = container.getBlobClient(blobName);
         if (!blobClient.exists()) {
             throw new RuntimeException("Blob not found: " + blobName);
         }
@@ -42,10 +66,48 @@ public class BlobStorageService {//TODO: change await to response time? to avoid
         }
     }
 
-    public void delete(String blobName) {
-        BlobClient blobClient = containerClient.getBlobClient(blobName);
+    private void deleteInternal(BlobContainerClient container, String blobName) {
+        BlobClient blobClient = container.getBlobClient(blobName);
         if (blobClient.exists()) {
             blobClient.delete();
         }
     }
+
+    public String uploadTaskFile(MultipartFile file) throws IOException {
+        return uploadInternal(taskFileContainer, file);
+    }
+
+    public byte[] downloadTaskFile(String blobName) throws IOException {
+        return downloadInternal(taskFileContainer, blobName);
+    }
+
+    public void deleteTaskFile(String blobName) {
+        deleteInternal(taskFileContainer, blobName);
+    }
+
+    public String uploadProfileImage(MultipartFile file) throws IOException {
+        return uploadInternal(profileImgContainer, file);
+    }
+
+    public void deleteProfileImage(String blobName) {
+        deleteInternal(profileImgContainer, blobName);
+    }
+
+    public String uploadGroupImage(MultipartFile file) throws IOException {
+        return uploadInternal(groupImgContainer, file);
+    }
+
+    public void deleteGroupImage(String blobName) {
+        deleteInternal(groupImgContainer, blobName);
+    }
+
+    public String uploadTaskImage(MultipartFile file) throws IOException {
+        return uploadInternal(taskImgContainer, file);
+    }
+
+    public void deleteTaskImage(String blobName) {
+        deleteInternal(taskImgContainer, blobName);
+    }
+
+
 }
