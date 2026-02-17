e22e7e507e4bb60fce7b9553d728991ffabbc4f5
Balasis_Ioannis <giovani1994a@gmail.com>
2026-02-17 05:56:42 +0200

    [fix] add date to deleted task in order to avoid returning them all

--- task-manager-backend/task-manager-engine/task-manager-engine-core/src/main/java/io/github/balasis/taskmanager/engine/core/repository/DeletedTaskRepository.java
+++ task-manager-backend/task-manager-engine/task-manager-engine-core/src/main/java/io/github/balasis/taskmanager/engine/core/repository/DeletedTaskRepository.java
@@ -6,13 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Set;
 
 @Repository
 public interface DeletedTaskRepository extends JpaRepository<DeletedTask, Long> {
 
-    @Query("SELECT dt.deletedTaskId FROM DeletedTask dt WHERE dt.group.id = :groupId")
-    Set<Long> findDeletedTaskIdsByGroupId(@Param("groupId") Long groupId);
+        @Query("""
+                SELECT dt.deletedTaskId
+                FROM DeletedTask dt
+                WHERE dt.group.id = :groupId
+                    AND dt.deletedAt > :since
+        """)
+        Set<Long> findDeletedTaskIdsByGroupIdAndDeletedAtAfter(@Param("groupId") Long groupId,
+                                                                                                                        @Param("since") Instant since);
 
     void deleteAllByGroup_Id(Long groupId);
 }
